22654de6bcd28b2e6e28d3ff19be0c00e28cdefb
laggu91 <laggu91@gmail.com>
2022-10-03 06:47:34 +0000

    edit env path
    Merge-request: TELESCOPE-MR-6
    Merged-by: 구원 라 <laggu91@gmail.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv'
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` })
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
 export const { NODE_ENV, PORT, SECRET_KEY, DB_URI, DB_NAME, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env
