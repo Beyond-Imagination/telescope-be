832c89e0b24417e0174cb868090adb3113d870cb
laggu91 <laggu91@gmail.com>
2022-11-13 11:58:14 +0000

    fix: get version from pacakge.json
    Merge-request: TELESCOPE-MR-55
    Merged-by: 구원 라 <laggu91@gmail.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 import { config } from 'dotenv'
+import { version } from '../../package.json'
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}` })
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
 export const { NODE_ENV, PORT, SECRET_KEY, DB_URI, DB_NAME, LOG_FORMAT, LOG_DIR, ORIGIN, SERVER_URL, CLIENT_URL } = process.env
-export const VERSION = process.env.npm_package_version
+export const VERSION = version
