44241faff99ecf480bac9e66ea38c3dcef2ac6ad
laggu91 <laggu91@gmail.com>
2023-04-09 03:17:54 +0000

    test: fix install version test code

--- src/test/libs/space.test.ts
+++ src/test/libs/space.test.ts
@@ -4,21 +4,19 @@ import v0_6_0 from '@/libs/space/version/v0.6.0'
 import { InvalidVersionException } from '@exceptions/InvalidVersionException'
 
 describe('space install version', () => {
-    const space = new Space()
-
     it('should get v0.5.0', () => {
-        expect(space.getInstallInfo('v0.5.0')).toBe(v0_5_0)
+        expect(Space.getInstallInfo('0.5.0')).toBe(v0_5_0)
     })
 
     it('should get v0.6.0', () => {
-        expect(space.getInstallInfo('v0.6.0')).toBe(v0_6_0)
+        expect(Space.getInstallInfo('0.6.0')).toBe(v0_6_0)
     })
 
     it('should get latest', () => {
-        expect(space.getInstallInfo('latest')).toBe(v0_6_0)
+        expect(Space.getInstallInfo('latest')).toBe(v0_6_0)
     })
 
     it('should throw error with unknown version', () => {
-        expect(() => space.getInstallInfo('v1.0.0')).toThrowError(InvalidVersionException)
+        expect(() => Space.getInstallInfo('1.0.0')).toThrowError(InvalidVersionException)
     })
 })
