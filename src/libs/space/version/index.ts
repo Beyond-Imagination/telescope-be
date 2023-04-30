c5529e0f6c2afba9bbdd263a0a2fe4164cd7b51d
laggu91 <laggu91@gmail.com>
2023-04-30 00:48:38 +0000

    feature: v0.7.0 install info
    Merge-request: TELESCOPE-MR-152
    Merged-by: 구원 라 <laggu91@gmail.com>

--- src/libs/space/version/index.ts
+++ src/libs/space/version/index.ts
@@ -1,7 +1,9 @@
 import v0_5_0 from './v0.5.0'
 import v0_6_0 from './v0.6.0'
+import v0_7_0 from './v0.7.0'
 export default {
     '0.5.0': v0_5_0,
     '0.6.0': v0_6_0,
-    latest: v0_6_0,
+    '0.7.0': v0_7_0,
+    latest: v0_7_0,
 }
---
+++ src/libs/space/version/v0.7.0.ts
@@ -0,0 +1,11 @@
+import { space } from '@/types/space.type'
+import v0_6_0 from './v0.6.0'
+
+const data: space.installInfo = {
+    version: '0.7.0',
+    webhooks: v0_6_0.webhooks,
+    uiExtension: v0_6_0.uiExtension,
+    right: v0_6_0.right,
+}
+
+export default data
