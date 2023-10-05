b0d700d7f16c28ea57af664264a42c59e99c4dc1
Inho Baek <inho.baek@nhn.com>
2023-10-05 15:40:26 +0000

    develop 배포 자동화
    :zap: Change startOn branch to develop
    
    :sparkles: Add automation code
    
    
    Co-authored-by: Inho Baek <inho.baek@nhn.com>
    
    
    Merge-request: TELESCOPE-MR-270
    Merged-by: 인호 백 <inhobaek.dan@gmail.com>

---
+++ .space.kts
@@ -0,0 +1,36 @@
+job("Backend Deploy 'develop'") {
+    startOn {
+        gitPush {
+            anyBranchMatching {
+                +"develop"
+            }
+        }
+    }
+
+    git {
+        refSpec {
+            +"refs/heads/develop"
+        }
+    }
+
+    container(displayName = "Push heroku remote", image = "alpine:3.18") {
+
+        env["HEROKU_API_KEY"] = "{{ project:HEROKU_API_KEY }}"
+
+        shellScript {
+            content = """
+                apk update
+                apk add git
+                echo "Git Version:"
+                git --version
+
+                git switch develop
+                echo "Git Status:"
+                git status
+
+                git remote add heroku https://token:${'$'}HEROKU_API_KEY@git.heroku.com/bi-telescope-api.git
+                git push heroku develop:main
+            """
+        }
+    }
+}
