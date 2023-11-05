f2118fef8a95ef0fba65f5c5256ea9c4582b58f0
konempty <1117plus@gmail.com>
2023-11-05 11:57:07 +0000

    release: v1.4.0

--- src/libs/space/version/index.ts
+++ src/libs/space/version/index.ts
@@ -4,6 +4,7 @@ import v0_7_0 from './v0.7.0'
 import v0_8_0 from './v0.8.0'
 import v1_1_0 from './v1.1.0'
 import v1_3_0 from './v1.3.0'
+import v1_4_0 from './v1.4.0'
 
 export default {
     '0.5.0': v0_5_0,
@@ -12,5 +13,6 @@ export default {
     '0.8.0': v0_8_0,
     '1.1.0': v1_1_0,
     '1.3.0': v1_3_0,
-    latest: v1_3_0,
+    '1.4.0': v1_4_0,
+    latest: v1_4_0,
 }
---
+++ src/libs/space/version/v1.4.0.ts
@@ -0,0 +1,11 @@
+import { space } from '@/types/space.type'
+import v1_3_0 from '@/libs/space/version/v1.3.0'
+
+const data: space.installInfo = {
+    version: '1.4.0',
+    webhooks: v1_3_0.webhooks,
+    uiExtension: v1_3_0.uiExtension,
+    right: v1_3_0.right,
+}
+
+export default data
